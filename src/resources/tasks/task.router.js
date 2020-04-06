9d8a1aa33dd659ba5fb51a746720fed8fac7561a
Furs Aleksey <wiemrid@gmail.com>
2020-04-06 21:50:04 +0300

    add GET, PUT, DELETE /boards/:id/tasks/:id

--- src/resources/tasks/task.router.js
+++ src/resources/tasks/task.router.js
@@ -5,8 +5,9 @@ const taskSchema = require('./task.schema');
 router
   .route('/')
   .get(async (req, res) => {
-    const tasks = await taskService.getAll();
-    res.status(200).json(tasks);
+    const { boardId } = req;
+    const boardTasks = await taskService.getAllBoardTask(boardId);
+    res.status(200).json(boardTasks);
   })
   .post(async (req, res, next) => {
     const newTaskData = {
@@ -19,6 +20,37 @@ router
     }
     const createdTask = await taskService.createTask(newTaskData);
     res.status(200).json(createdTask);
+  });
+
+router.param('id', async (req, res, next, id) => {
+  const foundedTask = await taskService.findTaskById(id);
+  if (foundedTask) {
+    req.task = foundedTask;
+    return next();
+  }
+  next(() => res.status(404).json('Not found'));
+});
+
+router
+  .route('/:id')
+  .get(async (req, res) => {
+    const { task } = req;
+    res.status(200).json(task);
+  })
+  .put(async (req, res, next) => {
+    const taskUpdateData = req.body;
+    const taskForUpdate = req.task;
+    const { error } = taskSchema.validate(taskUpdateData);
+    if (error) {
+      return next(() => res.status(400).json('Bad request'));
+    }
+    await taskService.updateTask(taskForUpdate, taskUpdateData);
+    return res.status(200).json('The task updated');
+  })
+  .delete(async (req, res) => {
+    const { task } = req;
+    await taskService.deleteTask(task);
+    res.status(200).json('The task deleted successfully');
   });
 
 router.use((err, req, res, next) => {
